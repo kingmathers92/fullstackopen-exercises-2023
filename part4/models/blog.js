768bea7833d0ebb3503f7216367dc77bbfc01cad
khaled <khaledb.yahya@gmail.com>
2024-02-17 23:51:08 +0100

    changed _id to id by default

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -8,6 +8,15 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 });
 
+// Define toJSON method to customize JSON representation
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString(); // Assign _id to id property
+    delete returnedObject._id; // Remove _id property
+    delete returnedObject.__v; // Remove __v property
+  }
+});
+
 const Blog = mongoose.model("Blog", blogSchema);
 
 module.exports = Blog;
